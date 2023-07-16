ec72241b5537fcf7675c21f251dae5662179b39d
Andrewphanpuspo <andrewphanpuspo2000@gmail.com>
2023-07-16 14:22:52 +1000

    first push

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const Port = process.env.PORT || 8000;
+//middleware
+app.use(cors());
+app.use(morgan());
+app.use(express.json());
+//database connection
+import mongoConnect from "./src/config/mongoConfig.js";
+mongoConnect();
+//api default
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "server is running",
+  });
+});
+app.listen(Port, (err) => {
+  err
+    ? console.log(err.message)
+    : console.log(`server is running at port ${Port}`);
+});
---
+++ src/config/mongoConfig.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const mongoConnect = async () => {
+  try {
+    const con = await mongoose.connect(process.env.MONGO_URI);
+    con && console.log(con);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default mongoConnect;
